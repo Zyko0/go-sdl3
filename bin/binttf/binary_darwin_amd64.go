65aedc00fee61269c175fb525dc54b535e83b71b
Zyko <13394516+Zyko0@users.noreply.github.com>
2025-03-09 17:48:11 +0100

    Test Xcode for sdl ttf macOS github workflow

--- bin/binttf/binary_darwin_amd64.go
+++ bin/binttf/binary_darwin_amd64.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	//go:embed assets/ttf_amd64.dylib
+	//go:embed assets/ttf.dylib
 	ttfBlob    []byte
 	ttfLibName = "libSDL3_ttf.dylib"
 )
--- bin/binttf/binary_darwin_arm64.go
+++ bin/binttf/binary_darwin_arm64.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	//go:embed assets/ttf_arm64.dylib
+	//go:embed assets/ttf.dylib
 	ttfBlob    []byte
 	ttfLibName = "libSDL3_ttf.dylib"
 )
