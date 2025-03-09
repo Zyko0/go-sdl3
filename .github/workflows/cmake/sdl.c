8f9c6f301d1e386a7b4ff77ec55f6776f9edfb5c
Zyko <13394516+Zyko0@users.noreply.github.com>
2025-03-09 22:44:30 +0100

    Emscripten build workflow

---
+++ .github/workflows/cmake/sdl.c
@@ -0,0 +1,4 @@
+#include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3_mixer/SDL_mixer.h>
+#include <SDL3_image/SDL_image.h>
--- bin/binttf/binary_darwin_arm64.go
+++ bin/binttf/binary_darwin_arm64.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	//go:embed assets/ttf.dylib
+	//go:embed assets/ttf_arm64.dylib
 	ttfBlob    []byte
 	ttfLibName = "libSDL3_ttf.dylib"
 )
