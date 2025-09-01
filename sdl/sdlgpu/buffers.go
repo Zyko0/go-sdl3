f8bd19dd167c4284bf9a1d9a000839609839821c
Zyko <13394516+Zyko0@users.noreply.github.com>
2025-09-01 18:26:16 +0200

    Try a reflection based approach

--- sdl/sdlgpu/buffers.go
+++ sdl/sdlgpu/buffers.go
@@ -1,131 +1,161 @@
 package sdlgpu
 
 import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
 	"unsafe"
-
-	"github.com/Zyko0/go-sdl3/sdl"
 )
 
-// GPUTransferBuffer
+// Test
 
-type TypedTransferBuffer[T any] struct {
-	buffer *sdl.GPUTransferBuffer
-	size   uint32
+type bufferItem struct {
+	ptr    reflect.Value
+	typ    reflect.Type
+	offset uint32
 	length uint32
 }
 
-// Raw returns the underlying GPUTransferBuffer.
-func (b *TypedTransferBuffer[T]) Raw() *sdl.GPUTransferBuffer {
-	return b.buffer
+type BaseLayout struct {
+	items []bufferItem
+	size  uint32
 }
 
-// Length returns the number of elements in the buffer.
-func (b *TypedTransferBuffer[T]) Length() uint32 {
-	return b.length
+func (l *BaseLayout) TotalSize() uint32 {
+	return l.size
 }
 
-// Size returns the total size of the buffer in bytes.
-func (b *TypedTransferBuffer[T]) Size() uint32 {
-	return b.size * b.length
+func (l *BaseLayout) Offset(slot, index uint32) uint32 {
+	addr := l.items[slot].offset
+	addr += index * uint32(l.items[slot].typ.Size())
+	return addr
 }
 
-func (b *TypedTransferBuffer[T]) Location(index uint32) *sdl.GPUTransferBufferLocation {
-	return &sdl.GPUTransferBufferLocation{
-		TransferBuffer: b.buffer,
-		Offset:         index * b.size,
+func (l *BaseLayout) Size(slot uint32) uint32 {
+	return max(l.items[slot].length, 1) * uint32(l.items[slot].typ.Size())
+}
+
+func (l *BaseLayout) Map(ptr uintptr) {
+	for _, item := range l.items {
+		addr := unsafe.Pointer(ptr + uintptr(item.offset))
+		if item.length > 0 {
+			item.ptr.Elem().Set(
+				reflect.SliceAt(item.typ, addr, int(item.length)),
+			)
+		} else {
+			item.ptr.Elem().Set(
+				reflect.NewAt(item.typ, addr),
+			)
+		}
 	}
 }
 
-func (b *TypedTransferBuffer[T]) Map(device *sdl.GPUDevice, cycle bool) ([]T, error) {
-	ptr, err := device.MapTransferBuffer(b.buffer, cycle)
-	if err != nil {
-		return nil, err
+func Register(layout any) {
+	t := reflect.TypeOf(layout)
+	if t.Kind() != reflect.Pointer {
+		panic("layout must be a pointer to a struct")
 	}
 
-	return unsafe.Slice((*T)(unsafe.Pointer(ptr)), b.length), nil
-}
-
-func CreateTypedTransferBuffer[T any](device *sdl.GPUDevice, usage sdl.GPUTransferBufferUsage, length uint32, props sdl.PropertiesID) (*TypedTransferBuffer[T], error) {
-	var t T
-
-	size := uint32(unsafe.Sizeof(t))
-	buffer, err := device.CreateTransferBuffer(&sdl.GPUTransferBufferCreateInfo{
-		Usage: usage,
-		Size:  size * length,
-		Props: props,
-	})
-	if err != nil {
-		return nil, err
+	t = t.Elem()
+	if t.Kind() != reflect.Struct {
+		panic("layout must be a pointer to a struct")
 	}
 
-	return &TypedTransferBuffer[T]{
-		buffer: buffer,
-		size:   size,
-		length: length,
-	}, nil
-}
-
-// GPUBuffer
-
-type TypedBuffer[T any] struct {
-	buffer *sdl.GPUBuffer
-	size   uint32
-	length uint32
-}
-
-// Raw returns the underlying GPUBuffer.
-func (b *TypedBuffer[T]) Raw() *sdl.GPUBuffer {
-	return b.buffer
-}
-
-// Length returns the number of elements in the buffer.
-func (b *TypedBuffer[T]) Length() uint32 {
-	return b.length
-}
-
-// Size returns the total size of the buffer in bytes.
-func (b *TypedBuffer[T]) Size() uint32 {
-	return b.size * b.length
-}
-
-func (b *TypedBuffer[T]) Location(index uint32) *sdl.GPUBufferLocation {
-	return &sdl.GPUBufferLocation{
-		Buffer: b.buffer,
-		Offset: index * b.size,
+	if t.NumField() > 0 && t.Field(0).Type != reflect.TypeOf(BaseLayout{}) {
+		panic("first field must be sdlgpu.BufferLayout")
 	}
-}
-
-func (b *TypedBuffer[T]) Binding(index uint32) *sdl.GPUBufferBinding {
-	return &sdl.GPUBufferBinding{
-		Buffer: b.buffer,
-		Offset: index * b.size,
-	}
-}
-
-func (b *TypedBuffer[T]) Region(start, end uint32) *sdl.GPUBufferRegion {
-	return &sdl.GPUBufferRegion{
-		Buffer: b.buffer,
-		Offset: start * b.size,
-		Size:   (end - start) * b.size,
-	}
-}
-
-func CreateTypedBuffer[T any](device *sdl.GPUDevice, usage sdl.GPUBufferUsageFlags, length uint32, props sdl.PropertiesID) (*TypedBuffer[T], error) {
-	var t T
-
-	size := uint32(unsafe.Sizeof(t))
-	buffer, err := device.CreateBuffer(&sdl.GPUBufferCreateInfo{
-		Usage: usage,
-		Size:  size * length,
-		Props: props,
-	})
-	if err != nil {
-		return nil, err
+	if t.NumField() < 2 {
+		panic("no buffer type to register")
 	}
 
-	return &TypedBuffer[T]{
-		buffer: buffer,
-		size:   size,
-		length: length,
-	}, nil
+	v := reflect.ValueOf(layout).Elem()
+	info := (*BaseLayout)(v.Field(0).Addr().UnsafePointer())
+	info.size = 0
+	info.items = make([]bufferItem, t.NumField()-1)
+
+	var slotsUsed bool
+	var offset uint32
+	for i := 1; i < v.NumField(); i++ {
+		fieldType := t.Field(i)
+		fieldValue := v.Field(i)
+
+		tag := fieldType.Tag.Get("gpu")
+		if tag == "" {
+			continue
+		}
+
+		// Tag parsing
+		fmt.Println("tag:", tag)
+		var slot, length = -1, -1
+		for _, decl := range strings.Split(tag, ",") {
+			parts := strings.Split(decl, ":")
+			switch parts[0] {
+			case "slot":
+				if len(parts) != 2 {
+					panic("invalid struct tag: " + decl)
+				}
+				n, err := strconv.Atoi(parts[1])
+				if err != nil {
+					panic("invalid slot number: " + parts[1])
+				}
+				if n < 0 {
+					panic("slot number must be >= 0: " + parts[1])
+				}
+				if n >= t.NumField()-1 {
+					panic("slot number out of range: " + parts[1])
+				}
+				slot = n
+			case "len":
+				if len(parts) != 2 {
+					panic("invalid struct tag: " + decl)
+				}
+				n, err := strconv.Atoi(parts[1])
+				if err != nil {
+					panic("invalid length: " + parts[1])
+				}
+				if n <= 0 {
+					panic("length must be positive: " + parts[1])
+				}
+				length = n
+			case "-":
+			default:
+				panic("unknown struct tag: " + parts[0])
+			}
+		}
+		// Set slot mode if first item
+		if i == 1 {
+			slotsUsed = slot >= 0
+		}
+		// Check slot mode consistency
+		if slotsUsed != (slot >= 0) {
+			panic("inconsistent slot mode in struct tags")
+		}
+		// Assign slot by struct field order if not specified explicitely
+		if !slotsUsed {
+			slot = i - 1
+		}
+		// Check if field is of either pointer of slice type
+		switch fieldValue.Kind() {
+		case reflect.Pointer:
+			length = 0
+		case reflect.Slice:
+			if length <= 0 {
+				panic("length must be specified for slices")
+			}
+		default:
+			panic("field must be a pointer or a slice: " + fieldType.Name)
+		}
+
+		info.items[slot] = bufferItem{
+			ptr:    fieldValue.Addr(),
+			typ:    fieldType.Type.Elem(),
+			offset: offset,
+			length: uint32(length),
+		}
+		// Increase offset by field size (or slice element size times length)
+		size := uint32(fieldType.Type.Elem().Size()) * uint32(max(length, 1))
+		info.size += size
+		offset += size
+	}
 }
