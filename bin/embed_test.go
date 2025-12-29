024914c4601da9df4cb6d9fa89c763bb9fc2b110
Zyko <13394516+Zyko0@users.noreply.github.com>
2025-12-29 20:13:36 +0100

    Try to split the tests

--- bin/embed_test.go
+++ bin/embed_test.go
@@ -1,7 +1,6 @@
 package bin_test
 
 import (
-	"runtime"
 	"runtime/debug"
 	"testing"
 
@@ -13,88 +12,87 @@ import (
 	"github.com/Zyko0/go-sdl3/ttf"
 )
 
-func Test_EmbeddedBinaries(t *testing.T) {
+func init() {
 	debug.SetPanicOnFault(true)
+}
 
-	t.Log("OS:", runtime.GOOS, "Arch:", runtime.GOARCH)
-	t.Run("SDL", func(t *testing.T) {
-		t.Run("Init", func(t *testing.T) {
-			defer binsdl.Load().Unload()
-			defer sdl.Quit()
+func Test_SDL(t *testing.T) {
+	t.Run("Init", func(t *testing.T) {
+		defer binsdl.Load().Unload()
+		defer sdl.Quit()
 
-			err := sdl.Init(0)
-			if err != nil {
-				t.Log(err)
-				t.FailNow()
-			}
+		err := sdl.Init(0)
+		if err != nil {
+			t.Log(err)
+			t.FailNow()
+		}
 
-			v := sdl.GetVersion()
-			t.Log("SDL version:", v.String())
-		})
+		v := sdl.GetVersion()
+		t.Log("SDL version:", v.String())
 	})
+}
 
-	t.Run("SDL_ttf", func(t *testing.T) {
-		t.Run("Init", func(t *testing.T) {
-			defer binsdl.Load().Unload()
-			defer binttf.Load().Unload()
-			defer sdl.Quit()
-			defer ttf.Quit()
+func Test_SDL_ttf(t *testing.T) {
+	t.Run("Init", func(t *testing.T) {
+		defer binsdl.Load().Unload()
+		defer binttf.Load().Unload()
+		defer sdl.Quit()
+		defer ttf.Quit()
 
-			err := sdl.Init(0)
-			if err != nil {
-				t.Log(err)
-				t.FailNow()
-			}
-			err = ttf.Init()
-			if err != nil {
-				t.Log(err)
-				t.FailNow()
-			}
-			v := ttf.GetVersion()
-			t.Log("SDL_ttf version:", v.String())
-		})
+		err := sdl.Init(0)
+		if err != nil {
+			t.Log(err)
+			t.FailNow()
+		}
+		err = ttf.Init()
+		if err != nil {
+			t.Log(err)
+			t.FailNow()
+		}
+		v := ttf.GetVersion()
+		t.Log("SDL_ttf version:", v.String())
 	})
+}
 
-	// TODO: mixer
-	/*t.Run("SDL_mixer", func(t *testing.T) {
-		t.Run("Init", func(t *testing.T) {
-			defer binsdl.Load().Unload()
-			defer binmix.Load().Unload()
-			defer sdl.Quit()
+// TODO: mixer
+/*t.Run("SDL_mixer", func(t *testing.T) {
+	t.Run("Init", func(t *testing.T) {
+		defer binsdl.Load().Unload()
+		defer binmix.Load().Unload()
+		defer sdl.Quit()
 
-			err := sdl.Init(0)
-			if err != nil {
-				t.Log(err)
-				t.FailNow()
-			}
-			err = mixer.Init()
-			if err != nil {
-				t.Log(err)
-				t.FailNow()
-			}
-			v := mix.GetVersion()
-			t.Log("SDL_mixer version:", v.String())
-		})
-	})*/
+		err := sdl.Init(0)
+		if err != nil {
+			t.Log(err)
+			t.FailNow()
+		}
+		err = mixer.Init()
+		if err != nil {
+			t.Log(err)
+			t.FailNow()
+		}
+		v := mix.GetVersion()
+		t.Log("SDL_mixer version:", v.String())
+	})
+})*/
 
-	t.Run("SDL_image", func(t *testing.T) {
-		t.Run("Init", func(t *testing.T) {
-			t.Log("before load unload")
-			defer binsdl.Load().Unload()
-			defer binimg.Load().Unload()
-			defer sdl.Quit()
+func Test_SDL_Image(t *testing.T) {
+	t.Run("Init", func(t *testing.T) {
+		t.Log("before load unload")
+		defer binsdl.Load().Unload()
+		defer binimg.Load().Unload()
+		defer sdl.Quit()
 
-			t.Log("before init")
-			err := sdl.Init(0)
-			if err != nil {
-				t.Log(err)
-				t.FailNow()
-			}
+		t.Log("before init")
+		err := sdl.Init(0)
+		if err != nil {
+			t.Log(err)
+			t.FailNow()
+		}
 
-			t.Log("before version")
-			v := img.GetVersion()
-			t.Log("after version")
-			t.Log("SDL_image version:", v.String())
-		})
+		t.Log("before version")
+		v := img.GetVersion()
+		t.Log("after version")
+		t.Log("SDL_image version:", v.String())
 	})
 }
